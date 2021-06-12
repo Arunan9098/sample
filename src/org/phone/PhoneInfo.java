27738f8da70ff92b52c57f67205570fd69b3eb45
Arunan9098 <arunan7durai@gmail.com>
2021-06-12 11:23:49 +0530

    1st commit

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,32 @@
+package org.phone;
+
+public class PhoneInfo {
+private void phoneName() {
+	// TODO Auto-generated method stub
+System.out.println("phoneName");
+}
+private void phoneMieiNum() {
+	// TODO Auto-generated method stub
+System.out.println("phoneMieiNum");
+}
+private void camera() {
+	// TODO Auto-generated method stub
+System.out.println("camera");
+}
+private void storage() {
+	// TODO Auto-generated method stub
+System.out.println("storage");
+}
+private void osName() {
+	// TODO Auto-generated method stub
+System.out.println("osName");
+}
+public static void main(String[] args) {
+	PhoneInfo pi = new PhoneInfo();
+	pi.phoneName();
+	pi.phoneMieiNum();
+	pi.camera();
+	pi.storage();
+	pi.osName();
+}
+}
